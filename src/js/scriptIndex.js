ba2b7048f982bc361b92ce31ddf12809636f1c91
A PLCQ <plumecocq.augustin@gmail.com>
2023-09-15 09:36:41 +0200

    login

--- src/js/scriptIndex.js
+++ src/js/scriptIndex.js
@@ -3,32 +3,22 @@ let Player;
 let Eror = document.getElementById('notValidEnter');
 
 function startGame(){
-
+    const forbiddenCharacters = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+  
     let namePlayerInput = document.getElementById("inputNamePlayer");
 
     Player = namePlayerInput.value;
 
-    if(Player.length < 3){
-        console.log("Nom du joueur trop court");
+    if (Player.length < 3) {
         Eror.textContent = "Nom du joueur trop court : 3 MIN";
         Eror.style.color = "red";
-    }
-
-    else if(Player.length >16){
-        console.log("Nom du joueur trop long");
+      } else if (forbiddenCharacters.test(Player)) {
+        Eror.textContent = "Nom du joueur contient des caractères interdits";
+        Eror.style.color = "red";
+        } else if (Player.length > 20) {
         Eror.textContent = "Nom du joueur trop long : 20 MAX";
         Eror.style.color = "red";
-    } else{
-        playerCreat =true;
-    }
-
-    if(playerCreat === true){ // condition si joueur crée & enemy crée
-    localStorage.setItem('user', Player);
-    console.log("name : " + Player);
-    //localStorage.setItem('enemy', Ennemy); // Stocker le nom des ennemy
-    //localStorage.setItem('user', Player); // stcoker les info du joueur
-    window.location.href = "/home.html";
-    } else{
-        console.log("Vous devez remplir le formulaire pour pouvoir continuer");
+      } else {
+        window.location.href = "/home.html";
+      }
   }
-}
